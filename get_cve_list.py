03bc2fa49b42fc5086e77763903e5e0988b19d4e
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2023-04-02 13:32:44 -0700

    Update get_cve_list.py

--- get_cve_list.py
+++ get_cve_list.py
@@ -1,28 +1,38 @@
-name: Update CVE list
+import requests
+import csv
+import time
 
-on:
-  schedule:
-    - cron: "0 0 * * *"
+# define the base URL and parameters for the NVD API
+base_url = "https://services.nvd.nist.gov/rest/json/cves/1.0"
+results_per_page = 2000
+start_index = 0
 
-jobs:
-  update-cve-list:
-    runs-on: ubuntu-latest
-    steps:
-      - name: Checkout code
-        uses: actions/checkout@v2
-      - name: Set up Python
-        uses: actions/setup-python@v2
-        with:
-          python-version: "3.x"
-      - name: Install dependencies
-        run: pip install requests
-      - name: Run Python script
-        run: python get_cve_list.py
-      - name: Commit changes
-        uses: EndBug/add-and-commit@v7
-        with:
-          author_name: "GitHub Actions"
-          author_email: "actions@github.com"
-          message: "Update CVE list"
-          add: "cve_list.csv"
-          branch: "cve-list-updates"
+# create a CSV file and write the header row
+with open("cve_list.csv", "w", newline="") as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(["CVE ID", "Description", "Published Date"])
+
+    # loop through the pages in the NVD API until all results are retrieved
+    while True:
+        params = {"resultsPerPage": str(results_per_page), "startIndex": str(start_index)}
+        response = requests.get(base_url, params=params)
+        cves = response.json()
+
+        # check if there are any more CVEs to retrieve
+        if len(cves["result"]["CVE_Items"]) == 0:
+            break
+
+        # write each CVE to the CSV file
+        for cve in cves["result"]["CVE_Items"]:
+            cve_id = cve["cve"]["CVE_data_meta"]["ID"]
+            description = cve["cve"]["description"]["description_data"][0]["value"]
+            published_date = cve["publishedDate"]
+            writer.writerow([cve_id, description, published_date])
+
+        # increment the start index for the next page
+        start_index += results_per_page
+
+        # pause for 1 second in between requests to avoid overloading the API
+        time.sleep(1)
+
+print("CVE list exported to cve_list.csv")
